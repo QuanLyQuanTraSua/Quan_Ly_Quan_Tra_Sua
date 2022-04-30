a11812d9a210ba28f38bce7cdaeb7dcd5bda4191
dangninh24 <89661088+dangninh24@users.noreply.github.com>
2022-04-30 12:48:10 +0700

    kết nối lại sql server

--- DAO/DataProvider.cs
+++ DAO/DataProvider.cs
@@ -21,7 +21,7 @@ namespace Phan_Mem_Quan_Ly_Quan_Tra_Sua.DAO
         private DataProvider() { }
 
 
-        private string connectionSTR = @"Data Source=localhost,1433;Initial Catalog=QLQuanTS;User ID=sa;Password=ninh2405";
+        private string connectionSTR = @"Data Source=117.1.195.58,1433;Initial Catalog=QLQuanTS;User ID=client;Password=ninh2405";
 
         public DataTable ExecuteQuery(string query, object[] parameter = null)
         {
